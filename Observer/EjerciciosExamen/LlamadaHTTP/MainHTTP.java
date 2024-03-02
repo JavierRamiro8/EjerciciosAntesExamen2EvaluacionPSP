5a5bf1a766f2fa84e1cc2fc4fa38eb821912693b
JavierRamiro8 <jramiroc04@gmail.com>
2024-03-02 19:32:13 +0100

    llamada HTTP funciona!!

---
+++ Observer/EjerciciosExamen/LlamadaHTTP/MainHTTP.java
@@ -0,0 +1,40 @@
+package EjerciciosExamen.LlamadaHTTP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MainHTTP {
+    private static final int RESOURCE_POSITION = 1;
+
+    public static void main(String[] args) {
+        int puerto = 8877;
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(puerto);
+            Socket connCliente = serverSocket.accept();
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(
+                            connCliente.getInputStream()));
+            String header = reader.readLine();
+            System.out.println(header);
+            // GET ________ HTTP/1.1
+            String info = extraeInformacion(header);
+            String[] numeros = info.split("/");
+            int n = Integer.parseInt(numeros[1]);
+            int m = Integer.parseInt(numeros[2]);
+            PrimosHTTP primos = new PrimosHTTP(m, n, serverSocket);
+            Logger logeado = new Logger(connCliente,m);
+            primos.agregarObservador(logeado);
+            primos.generarNumerosPrimos(n, m);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String extraeInformacion(String header) {
+        return header.split(" ")[RESOURCE_POSITION];
+    }
+}
