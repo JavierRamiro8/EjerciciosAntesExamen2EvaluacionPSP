4f29371747222f04299669ddb15e50a3d8c11561
JavierRamiro8 <jramiroc04@gmail.com>
2024-03-02 16:13:31 +0100

    1

--- Observer/EjerciciosExamen/EjercicioCuadrado/MainCuadrado.java
+++ Observer/EjerciciosExamen/EjercicioCuadrado/MainCuadrado.java
@@ -1,8 +1,10 @@
 package EjerciciosExamen.EjercicioCuadrado;
+
 public class MainCuadrado {
+
     public static void main(String[] args) {
-        int puertoReceptor=Integer.parseInt(args[1]);
-        int puertoEnviador=Integer.parseInt(args[2]);
+        int puertoReceptor=Integer.parseInt(args[0]);
+        int puertoEnviador=Integer.parseInt(args[1]);
         Receptor receptor=new Receptor(puertoReceptor);
         Enviador enviador=new Enviador(puertoEnviador);
         receptor.agregarObservador(enviador);
